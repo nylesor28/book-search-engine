a086d2efbda139c06455d35dea9509cb78e7a24d
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-12 23:11:31 -0400

    set up apollo in server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,34 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+
+
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const startServer = async () => {
+
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    // context: authMiddleware
+
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,6 +38,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+//TODO : DELETE USE ROUTES
 app.use(routes);
 
 db.once('open', () => {
