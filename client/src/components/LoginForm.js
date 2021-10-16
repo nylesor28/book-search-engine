af6246a27268abc975a19db27b3b2297c621ad79
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-16 12:18:58 -0400

    client login logic

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,15 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
+//TODO: REMOVE : import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -26,15 +29,23 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      //TODO REMOVE
+      // const response = await loginUser(userFormData);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
+      const { data } = await login({
+        variables: { ...userFormData }
+      });
+  
+      Auth.login(data.login.token);
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
