7da7810032f90848a80490ad0b8f1c288dc4032f
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-17 01:01:45 -0400

    remove unused code from SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -4,7 +4,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -32,15 +31,7 @@ const SignupForm = () => {
     }
 
     try {
-      // const response = await createUser(userFormData);
 
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
       const { data } = await addUser({
         variables: { ...userFormData }
       });
