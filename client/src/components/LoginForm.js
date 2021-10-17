99f26915e3546e00137f7eff150626964cd7cef2
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-17 00:56:26 -0400

    remove unused code from LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,10 +1,10 @@
-// see SignupForm.js for comments
+
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
-//TODO: REMOVE : import { loginUser } from '../utils/API';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -30,16 +30,6 @@ const LoginForm = () => {
 
     try {
 
-      //TODO REMOVE
-      // const response = await loginUser(userFormData);
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
       const { data } = await login({
         variables: { ...userFormData }
       });
